2ae4d84dc23ee9adcf77139295b341772d5a031a
cstarkjp <cstarkjp@gmail.com>
2025-09-08 10:14:22 +0900

    Tidying

--- src/nonlinear.cpp
+++ src/nonlinear.cpp
@@ -7,31 +7,8 @@
 
 #include "base.hpp"
 
-// // User-defined variables
-// double quadratic_coeff;
-// double D;
+// Dummy definitions, to be replaced by application-specific versions
 
-void Dornic::set_nonlinear_coefficients(const Coefficients &f_coefficients)
-{
-    // quadratic_coeff = f_coefficients.quadratic;
-    // D = f_coefficients.diffusion / (dx*dx);
-}
-
+void Dornic::set_nonlinear_coefficients(const Coefficients &f_coefficients) {}
 auto Dornic::nonlinear_rhs(const int i_node, const dbl_vector &field) 
-    const -> double
-{
-    // // Non-linear terms
-    // const double quadratic_term = -quadratic_coeff*field[i_node]*field[i_node];
-
-    // // Integration of diffusion
-    // double diffusion_sum = 0.0;
-    // int n_neighbors = neighbors[i_node].size();
-    // for (auto i=0; i<n_neighbors; i++)
-    // {
-    //     auto i_neighbor = neighbors[i_node][i];
-    //     diffusion_sum += field[i_neighbor];
-    // }
-    // diffusion_sum = D*(diffusion_sum - n_neighbors*field[i_node]);
-    // return diffusion_sum + quadratic_term;
-    return 0.0;
-}
+    const -> double {}
