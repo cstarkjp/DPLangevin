89b2ebf517a2dcba7fa65b2b42dc3ce5968239ba
cstarkjp <cstarkjp@gmail.com>
2025-09-12 19:28:10 +0800

    Adapted to work with rectangular grid; not tested yet

--- src/grid2d.cpp
+++ src/grid2d.cpp
@@ -9,11 +9,9 @@
 
 void DornicBase::construct_2D_grid(const Parameters parameters)
 {
-    int up, down, right, left;
-    // const int L = sqrt(cell_density.size());
+    int top_row, bottom_row, right_column, left_column;
     const int n_x = parameters.grid_size.at(0);
     const int n_y = parameters.grid_size.at(1);
-    const int L = n_x;
     std::cout << "n_x: " << n_x << std::endl;
     std::cout << "n_y: " << n_y << std::endl;
 
@@ -26,21 +24,16 @@ void DornicBase::construct_2D_grid(const Parameters parameters)
         {
             for (auto x=0; x<n_x; x++)
             {
-                up = (y < L-1) ? x + (y+1)*L : x;
-                down = (y > 0) ? x + (y-1)*L : x + (L-1)*L;
-                right = (x < L-1) ? x+1 + y*L : y*L;
-                left = (x > 0) ? x-1 + y*L : L-1 + y*L;
-
-                // up = (y < n_y-1) ? x + (y+1)*n_x : x;
-                // down = (y > 0) ? x + (y-1)*n_x : x + (L-1)*n_x;
-                // right = (x < L-1) ? x+1 + y*n_x : y*n_x;
-                // left = (x > 0) ? x-1 + y*n_x : n_x-1 + y*n_x;
-
                 auto i = x + y*n_x;
-                neighbors[i][0] = up;    // Up
-                neighbors[i][1] = down;  // Down
-                neighbors[i][2] = right; // Left (CPS:??)
-                neighbors[i][3] = left;  // Right (CPS:??)
+                top_row      = (y < n_y-1) ? x + (y+1)*n_x : x;
+                bottom_row   = (y > 0) ? x + (y-1)*n_x : x + (n_y-1)*n_x;
+                right_column = (x < n_x-1) ? x+1 + y*n_x : y*n_x;
+                left_column  = (x > 0) ? x-1 + y*n_x : n_x-1 + y*n_x;
+
+                neighbors[i][0] = top_row;    // Up
+                neighbors[i][1] = bottom_row;  // Down
+                neighbors[i][2] = right_column; // Left (CPS: huh??)
+                neighbors[i][3] = left_column;  // Right (CPS: huh??)
             }
         }
     }
@@ -54,51 +47,60 @@ void DornicBase::construct_2D_grid(const Parameters parameters)
                 // i is the index of the flattened grid
                 auto i = x + y*n_x;
                 // Each cell has 4 neighbors[i] indexes
-                neighbors[i][0] = x + (y+1)*n_x;  // Up:   i + n_x
-                neighbors[i][1] = x + (y-1)*n_x;  // Down: i - n_x
-                neighbors[i][2] = (x+1) + y*n_x;  // Right: i+1   (VMB: left)
-                neighbors[i][3] = (x-1) + y*n_x;  // Left:  i-1   (VMB: right)
+                neighbors[i][0] = i + n_x;  // Up:   i + n_x // x + (y+1)*n_x;
+                neighbors[i][1] = i - n_x;  // Down: i - n_x // x + (y-1)*n_x;
+                neighbors[i][2] = i + 1;  // Right: i+1   (VMB: left)  // (x+1) + y*n_x;
+                neighbors[i][3] = i - 1;  // Left:  i-1   (VMB: right) // (x-1) + y*n_x;
             }
         }
-        for (auto i=1; i<L-1; i++)
+        // Top and bottom rows
+        for (auto i=1; i<n_x-1; i++)
         {
-            down  = i;             // Bottom row
-            up    = i + (L-1)*L;   // Top row
-            left  = i*L;           // Left column
-            right = (L-1) + i*L;   // Right column
+            bottom_row = i;             // Bottom row
+            top_row    = i + (n_y-1)*n_x;   // Top row
 
             // Each one of these have three neighbors, redefine
-            neighbors[down]  = int_vector(3);
-            neighbors[up]    = int_vector(3);
-            neighbors[left]  = int_vector(3);
-            neighbors[right] = int_vector(3);
+            neighbors[bottom_row] = int_vector(3);
+            neighbors[top_row]    = int_vector(3);
 
             // Set the neighbors
-            neighbors[down][0] = down + L;
-            neighbors[down][1] = down - 1; 
-            neighbors[down][2] = down + 1; 
+            neighbors[bottom_row][0] = bottom_row + n_x;
+            neighbors[bottom_row][1] = bottom_row - 1; 
+            neighbors[bottom_row][2] = bottom_row + 1; 
+            neighbors[top_row][0] = top_row - n_x;
+            neighbors[top_row][1] = top_row - 1; 
+            neighbors[top_row][2] = top_row + 1;
+        }
+        // Left and right columns
+        for (auto i=1; i<n_y-1; i++)
+        {
+            left_column  = i*n_x;           // Left column
+            right_column = (n_x-1) + i*n_x; // Right column
 
-            neighbors[up][0] = up - L;
-            neighbors[up][1] = up - 1; 
-            neighbors[up][2] = up + 1;
+            // Each one of these have three neighbors, redefine
+            neighbors[left_column]  = int_vector(3);
+            neighbors[right_column] = int_vector(3);
 
-            neighbors[left][0] = left + L;
-            neighbors[left][1] = left - L; 
-            neighbors[left][2] = left + 1;
-
-            neighbors[right][0] = right + L;
-            neighbors[right][1] = right - L; 
-            neighbors[right][2] = right - 1;
+            // Set the neighbors
+            neighbors[left_column][0] = left_column + n_x;
+            neighbors[left_column][1] = left_column - n_x; 
+            neighbors[left_column][2] = left_column + 1;
+            neighbors[right_column][0] = right_column + n_x;
+            neighbors[right_column][1] = right_column - n_x; 
+            neighbors[right_column][2] = right_column - 1;
         }
 
-        // Corners
-        neighbors[0][0] = 1; 
-        neighbors[0][1] = L;
-        neighbors[L-1][0] = L-2; 
-        neighbors[L-1][1] = (L-1)+L;
-        neighbors[(L-1)*L][0] = 1+(L-1)*L; 
-        neighbors[(L-1)*L][1] = (L-2)*L;
-        neighbors[(L-1)+(L-1)*L][0] = (L-2)+(L-1)*L; 
-        neighbors[(L-1)+(L-1)*L][1] = (L-1)+(L-2)*L;
+        // Bottom-left corner
+        neighbors[0][0] = 0 + 1; 
+        neighbors[0][1] = 0 + n_x;
+        // Bottom-right corner
+        neighbors[n_x-1][0] = (n_x-1) - 1;
+        neighbors[n_x-1][1] = (n_x-1) + n_x;
+        // Top-left corner
+        neighbors[(n_y-1)*n_x][0] = (n_y-1)*n_x + 1; 
+        neighbors[(n_y-1)*n_x][1] = (n_y-2)*n_x;
+        // Top-right corner
+        neighbors[(n_x-1)+(n_y-1)*n_x][0] = (n_x-2)+(n_y-1)*n_x; 
+        neighbors[(n_x-1)+(n_y-1)*n_x][1] = (n_x-1)+(n_y-2)*n_x;
     }
 }
