c7f0ab4f6b4d372338932c2fbbc8e7daff1c9cce
cstarkjp <cstarkjp@gmail.com>
2025-09-15 11:27:06 +0800

    Rerun tests

--- test/test_dp.py
+++ test/test_dp.py
@@ -36,6 +36,6 @@ if not sim_dp.run():
 print(f"epochs:  {sim_dp.get_epochs()}")
 print(f"mean_densities:  {sim_dp.get_mean_densities()}")
 print()
-print("cell density grid  (not working yet):")
+print("cell density grid (at final t):")
 print(np.round(sim_dp.get_density().T, 2))
 
