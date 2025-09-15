37ce1b6dcb4f61c5b9dc057e6d547477be822384
cstarkjp <cstarkjp@gmail.com>
2025-09-15 12:59:27 +0800

    Adjust PyPI metadata

--- src/application_pybind.cpp
+++ src/application_pybind.cpp
@@ -14,7 +14,7 @@
 
 PYBIND11_MODULE(dplvn, module)
 {
-    module.attr("__version__") = "2025.09.15a4";
+    module.attr("__version__") = "2025.09.15a5";
     module.doc() = 
         "Operator-splitting method of integrating DP-type Langevin equations"; 
 
