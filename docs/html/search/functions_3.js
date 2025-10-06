d142c562403daa09fef06d8a31dfb2539c831806
cstarkjp <cstarkjp@gmail.com>
2025-10-06 21:26:56 +0800

    Add a macos15 build to the workflow

--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['euler_5fand_5fstochastic_0',['euler_and_stochastic',['../class_base_langevin.html#abf00674dbbd6ef4eafc2a6f3bf821212',1,'BaseLangevin']]]
+  ['euler_5fand_5fstochastic_0',['euler_and_stochastic',['../class_base_langevin.html#ae16115a582dc618d6184aa8b7a954b92',1,'BaseLangevin']]]
 ];
