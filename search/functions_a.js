cd4dbae5e93310f79a6b5a3655fff804a4c285d1
cstarkjp <cstarkjp@gmail.com>
2025-10-11 05:40:20 +0000

    Deploying to gh-pages from @ cstarkjp/DPLangevin@e62151c70db39b3689fe3a66cbef837f557b2094 🚀

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,8 +1,7 @@
 var searchData=
 [
-  ['set_5fcoefficients_0',['set_coefficients',['../classBaseLangevin.html#a7404d2313ab0a225c89bd64833a278ea',1,'BaseLangevin']]],
-  ['set_5fessential_5fcoefficients_1',['set_essential_coefficients',['../classBaseLangevin.html#af2ba5ff83a1d5221e4dd3f91782eb7ac',1,'BaseLangevin']]],
-  ['set_5flambdas_2',['set_lambdas',['../classBaseLangevin.html#a2df86bdadff3051b6e3d0b36a232cff6',1,'BaseLangevin']]],
-  ['set_5fnonlinear_5fcoefficients_3',['set_nonlinear_coefficients',['../classDPLangevin.html#a0008b416d0460cdcb34646733dae8156',1,'DPLangevin::set_nonlinear_coefficients()'],['../classBaseLangevin.html#adfcbb53015503e909b0b2c2b5549ae5b',1,'BaseLangevin::set_nonlinear_coefficients()']]],
-  ['simdp_4',['SimDP',['../classSimDP.html#a6f567ecf5366b0325f9789afbf7b0712',1,'SimDP']]]
+  ['set_5fessential_5fcoefficients_0',['set_essential_coefficients',['../classBaseLangevin.html#af2ba5ff83a1d5221e4dd3f91782eb7ac',1,'BaseLangevin']]],
+  ['set_5flambdas_1',['set_lambdas',['../classBaseLangevin.html#a2df86bdadff3051b6e3d0b36a232cff6',1,'BaseLangevin']]],
+  ['set_5fnonlinear_5fcoefficients_2',['set_nonlinear_coefficients',['../classDPLangevin.html#a0008b416d0460cdcb34646733dae8156',1,'DPLangevin::set_nonlinear_coefficients()'],['../classBaseLangevin.html#adfcbb53015503e909b0b2c2b5549ae5b',1,'BaseLangevin::set_nonlinear_coefficients()']]],
+  ['simdp_3',['SimDP',['../classSimDP.html#a6f567ecf5366b0325f9789afbf7b0712',1,'SimDP']]]
 ];
