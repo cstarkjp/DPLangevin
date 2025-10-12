c59414e8f11360dee348605cf6c641fd81152337
cstarkjp <cstarkjp@gmail.com>
2025-10-12 17:27:55 +0800

    Rerun nb

--- docs/html/search/all_b.js
+++ docs/html/search/all_b.js
@@ -4,6 +4,6 @@ var searchData=
   ['n_5fepochs_1',['n_epochs',['../class_sim_d_p.html#aa0794cf12da5c60de961f66d882bd00f',1,'SimDP']]],
   ['neighborhood_5ft_2',['neighborhood_t',['../langevin__types_8hpp.html#a62fa2c6d66d1424f015cf138943986b0',1,'langevin_types.hpp']]],
   ['noise_5fcoefficient_3',['noise_coefficient',['../class_base_langevin.html#a31e32de62ececd446824b3a413254f28',1,'BaseLangevin']]],
-  ['nonlinear_5frhs_4',['nonlinear_rhs',['../class_d_p_langevin.html#a94f89b991ae3fc7601a775525d4ea646',1,'DPLangevin::nonlinear_rhs()'],['../class_base_langevin.html#a2de82e54eb1098353615261ff681d4e6',1,'BaseLangevin::nonlinear_rhs()']]],
+  ['nonlinear_5frhs_4',['nonlinear_rhs',['../class_d_p_langevin.html#a2739bf17103f22840b99cee9b1d9092b',1,'DPLangevin::nonlinear_rhs()'],['../class_base_langevin.html#a2de82e54eb1098353615261ff681d4e6',1,'BaseLangevin::nonlinear_rhs()']]],
   ['notes_5',['Build notes',['..//Users/cps/Projects/DPLangevin/src/README.md#autotoc_md7',1,'']]]
 ];
